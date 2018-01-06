5c903455840b8bd6c7bb6262a4eed1b47e9da21b
wxl <826650562@qq.com>
2018-01-06 15:34:41 +0800

    ssss

--- WebContent/WEB-INF/rwjr/js/sjrz.js
+++ WebContent/WEB-INF/rwjr/js/sjrz.js
@@ -1,7 +1,7 @@
 $(function() {
 	var w_sjh = 0;
+	var is_tupianYanzheng=false;
 	function getYzm() {
-		//tpyzmsx
 		$("._yzm").attr("src", 'yzm');
 		$('.tpyzmsx').click(function() {
 			$("._yzm").attr("src", 'yzm?time=' + new Date().getTime());
@@ -10,23 +10,19 @@ $(function() {
 	getYzm();
 
 	$(".hqyzm").click(function() {
-		_msg("");
 		var _sjh = $.trim($("#_sjh").val());
 		var _txyzm = $.trim($("#_txyzm").val());
+		if (!checkOutSubmit(_sjh,_txyzm)) return false;
 		$.ajax({
 			url : "getyzm",
 			type : "post",
 			success : function(data) {
-				if (_sjh.length <= 0) {
-					//提示
-					_msg("*—* 手机号不能为空！");
-				} else if (!(/^1[3|4|5|8][0-9]\d{4,8}$/.test(_sjh)) || _sjh.length != 11) {
-					_msg("*—* 手机号不正确！");
-				} else if (_txyzm.length <= 0) {
-					_msg("*—* 图形验证码不能为空！");
-				} else if (_txyzm.toUpperCase() != data) {
+				if (_txyzm.toUpperCase() != data) {
 					_msg("*—* 图形验证码不正确！");
+					is_tupianYanzheng=false;
+					return false;
 				} else {
+					is_tupianYanzheng=true;
 					//发送手机验证码
 					w_sjh = _sjh;
 					$.ajax({
@@ -58,7 +54,10 @@ $(function() {
 	});
 
 	$("#_login").click(function() {
-		_msg("");
+		var _sjh = $.trim($("#_sjh").val());
+		var _txyzm = $.trim($("#_txyzm").val());
+		if (!checkOutSubmit(_sjh,_txyzm)) return false;
+		if (!is_tupianYanzheng) _msg("*—* 请输入正确的图片验证码!");
 		var dxyzm = $.trim($("#dxyzm").val());
 		if (dxyzm.length <= 0) {
 			_msg("*—* 请输入短信验证码!");
@@ -86,6 +85,20 @@ $(function() {
 		}
 	});
 
-
+	function checkOutSubmit(_sjh,_txyzm) {
+		if (_sjh.length <= 0) {
+			//提示
+			_msg("*—* 手机号不能为空！");
+			return false;
+		} else if (!(/^1[3|4|5|8][0-9]\d{4,8}$/.test(_sjh)) || _sjh.length != 11) {
+			_msg("*—* 手机号不正确！");
+			return false;
+		} else if (_txyzm.length <= 0) {
+			_msg("*—* 图形验证码不能为空！");
+			return false;
+		} else {
+			return true
+		}
+	}
 
 })
